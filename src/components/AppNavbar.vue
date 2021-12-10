ebc2d01c6b244f3c61d2b780af4c29976a5d0bfc
TheAkshitS <akshit.s@outlook.com>
2021-12-10 09:09:04 +0530

    refactor: AppNavbar

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav v-if="!isLoginPage">
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <div class="text-uppercase">Movies</div>
+      </div>
+
+      <v-spacer />
+
+      <v-btn icon class="mr-1" @click="logout">
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "AppNavbar",
+
+  computed: {
+    isLoginPage() {
+      return this.$route.name === "Login";
+    },
+  },
+
+  methods: {
+    logout() {
+      this.$router.push('/login');
+    },
+  },
+};
+</script>
