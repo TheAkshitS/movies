249f261ce59e4a12a302f880972d3f12b92f9005
TheAkshitS <akshit.s@outlook.com>
2021-12-10 11:55:24 +0530

    refactor: for movie component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,45 @@
 <template>
-  <hello-world />
+  <v-container fluid grid-list-xl class="mt-10">
+    <movie-search />
+
+    <movie-list :movies="movies" />
+  </v-container>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+import MovieService from '../services/MovieService.js';
+
+import MovieSearch from '@/components/MovieSearch.vue';
+import MovieList from '@/components/MovieList.vue';
 
 export default {
-  name: "Home",
+  name: 'Home',
 
   components: {
-    HelloWorld,
+    MovieSearch,
+    MovieList,
   },
-};
+
+  data() {
+    return {
+      movies: [],
+    }
+  },
+
+  created() {
+    this.getFeaturedMovies();
+  },
+
+  methods: {
+    async getFeaturedMovies() {
+      try {
+        const response = await MovieService.getFeaturedMovies();
+
+        this.movies = response.data.results; 
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  }
+}
 </script>
