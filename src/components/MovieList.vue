52e2cbb48db6406093e443b26ecfd03ed9b7858a
TheAkshitS <akshit.s@outlook.com>
2021-12-10 13:05:02 +0530

    fix: movie iteration

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,7 +1,7 @@
 <template>
   <v-layout row wrap>
-    <v-flex xs12 sm6 md4 lg3 v-for="movie in movies" :key="movie.name">
-      <movie-card :movie="movie" v-for="(movie, i) in movies" :key="i" />
+    <v-flex xs12 sm6 md4 lg3 v-for="movie in movies" :key="movie.id">
+      <movie-card :movie="movie" />
     </v-flex>
   </v-layout>
 </template>
